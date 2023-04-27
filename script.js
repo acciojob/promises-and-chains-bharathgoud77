c384da25439628f52a5f20da8712791217f2a84c
bharathgoud77 <92040880+bharathgoud77@users.noreply.github.com>
2023-04-27 17:48:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 //your JS code here. If required.
-let promise = new Promise(resolve, reject)
-{
-	setTimeout(function(){
-		var age= document.getElementById("age").value;
-		var name = document.getElementById("name").value;
-		if(age >= 18){
-			window.alert(`Welcome, ${name}. You can vote.`)
-		}
-		else{
-			window.alert(`Oh sorry ${name}. You aren't old enough`)
-		}
-	},4000}
+function data(){
+	setTimeout(()=>{
+		var age = document.getElementById("age").value;
+	var name = document.getElementById("name").value;
+	if(age>=18){
+		window.alert(`Welcome, ${name}. You can vote.`)
+	}else{
+		window.alert(`Oh sorry ${name}. You aren't old enough.`)
+	}
+	},4000)
 }
